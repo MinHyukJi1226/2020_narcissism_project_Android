81064e8e4fd926c6171fcb894c9fdd8794245152
dmschd0678 <63108606+dmschd0678@users.noreply.github.com>
2020-12-02 20:32:35 +0900

    create category,search
    검색화면 만듬
    카테고리 화면 만듬
    기능 X

---
+++ Narcissism_Android/app/src/main/java/org/techtown/narcissism_android/SearchingActivity.java
@@ -0,0 +1,14 @@
+package org.techtown.narcissism_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searching);
+    }
+}
