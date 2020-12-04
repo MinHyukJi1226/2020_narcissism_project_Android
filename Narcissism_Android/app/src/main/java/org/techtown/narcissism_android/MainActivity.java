9a39cb2ad66c2c1ae8dc6c80e6d3bf244d0b6826
MinHyukJi1226 <63153516+MinHyukJi1226@users.noreply.github.com>
2020-12-04 20:48:43 +0900

    add Search, Option

--- Narcissism_Android/app/src/main/java/org/techtown/narcissism_android/MainActivity.java
+++ Narcissism_Android/app/src/main/java/org/techtown/narcissism_android/MainActivity.java
@@ -2,10 +2,13 @@ package org.techtown.narcissism_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -19,12 +22,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button buttonClothes = (Button) findViewById(R.id.buttonClothes);
         Button buttonInterior = (Button) findViewById(R.id.buttonInterior);
         Button buttonTip = (Button) findViewById(R.id.buttonTip);
+        ImageView imageSearch = findViewById(R.id.imageSearch);
+        ImageView imageOption = findViewById(R.id.imageOption);
 
         buttonClean.setOnClickListener(this);
         buttonRecycle.setOnClickListener(this);
         buttonClothes.setOnClickListener(this);
         buttonInterior.setOnClickListener(this);
         buttonTip.setOnClickListener(this);
+        imageSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SearchingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        imageOption.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), OptionActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -35,7 +55,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.buttonRecycle: id = 2; break;
             case R.id.buttonClothes: id = 3; break;
             case R.id.buttonInterior: id = 4; break;
-            case R.id.buttonTip: id = 5; break; 
+            case R.id.buttonTip: id = 5; break;
         }
         if(id > 0){
             Intent intent = new Intent(getApplicationContext(), CategoryActivity.class);
