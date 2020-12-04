a709b46cdc96f4caad07a92434a387689269e643
MinHyukJi1226 <63153516+MinHyukJi1226@users.noreply.github.com>
2020-12-04 17:24:09 +0900

    Update MainActivity & Fix OptionActivity

--- Narcissism_Android/app/src/main/java/org/techtown/narcissism_android/MainActivity.java
+++ Narcissism_Android/app/src/main/java/org/techtown/narcissism_android/MainActivity.java
@@ -2,13 +2,45 @@ package org.techtown.narcissism_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonClean = (Button) findViewById(R.id.buttonClean);
+        Button buttonRecycle = (Button) findViewById(R.id.buttonRecycle);
+        Button buttonClothes = (Button) findViewById(R.id.buttonClothes);
+        Button buttonInterior = (Button) findViewById(R.id.buttonInterior);
+        Button buttonTip = (Button) findViewById(R.id.buttonTip);
+
+        buttonClean.setOnClickListener(this);
+        buttonRecycle.setOnClickListener(this);
+        buttonClothes.setOnClickListener(this);
+        buttonInterior.setOnClickListener(this);
+        buttonTip.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = 0;
+        switch (view.getId()){
+            case R.id.buttonClean: id = 1; break;
+            case R.id.buttonRecycle: id = 2; break;
+            case R.id.buttonClothes: id = 3; break;
+            case R.id.buttonInterior: id = 4; break;
+            case R.id.buttonTip: id = 5; break; 
+        }
+        if(id > 0){
+            Intent intent = new Intent(getApplicationContext(), CategoryActivity.class);
+            intent.putExtra("categoryId", id);
+            startActivity(intent);
+        }
     }
 }
