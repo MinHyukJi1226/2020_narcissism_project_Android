853208c40b02ff28f2d79ac4c4bf10e53ec38658
dmschd0678 <63108606+dmschd0678@users.noreply.github.com>
2020-12-04 19:18:33 +0900

    create recyclerView, fix CategoryActivity
    recyclerView 만듬
    카테고리 선택한 거 나오게 만듬...

---
+++ Narcissism_Android/app/src/main/java/org/techtown/narcissism_android/Data.java
@@ -0,0 +1,32 @@
+package org.techtown.narcissism_android;
+
+public class Data {
+
+    private String title;
+    private String content;
+    private int image;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public int getImage() {
+        return image;
+    }
+}
